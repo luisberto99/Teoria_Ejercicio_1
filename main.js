d52087fd0fd4d1b03577bef4a6c6107749c9d4b5
Alberto Medina <luisitomedina95@gmail.com>
2021-11-22 19:00:07 -0600

    Calculo de población y tasa de fecundidad

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
+let poblacion = [];
+
 function calcular() {
-    var poblacion = [];
-    var numeroAnhos = document.getElementById("input_anhos").value
-    var poblacionMasculina = document.getElementById("input_masc").value
-    var poblacionFemenina = document.getElementById("input_fem").value
-    var mortalidadMasculina = document.getElementById("tasa_mort_masc").value / 100
-    var mortalidadFemenina = document.getElementById("tasa_mort_fem").value / 100
-    var natalidad = document.getElementById("tasa_nat").value / 100
+    poblacion = [];
+    var numeroAnhos = parseInt(document.getElementById("input_anhos").value)
+    var poblacionMasculina = parseInt(document.getElementById("input_masc").value)
+    var poblacionFemenina = parseInt(document.getElementById("input_fem").value)
+    var mortalidadMasculina = parseInt(document.getElementById("tasa_mort_masc").value) / 100
+    var mortalidadFemenina = parseInt(document.getElementById("tasa_mort_fem").value) / 100
+    var natalidadMasculina = parseInt(document.getElementById("tasa_nat_masc").value) / 100
+    var natalidadFemenina = parseInt(document.getElementById("tasa_nat_fem").value) / 100
+    var poblacionFertil = parseInt(document.getElementById("num_mujeres_fertil").value)
 
     //TODO VERIFICAR QUE TODOS LOS CAMPOS SEAN VALIDOS
 
-    console.log(calculoPoplacion(numeroAnhos, poblacionMasculina, natalidad, mortalidadMasculina));
+    console.log(calculoPoplacion(numeroAnhos, poblacionMasculina, natalidadMasculina, mortalidadMasculina));
+    console.log(poblacion);
+    numNacimientos = poblacionMasculina * natalidadMasculina + poblacionFemenina * natalidadFemenina;
+    console.log(calculoFecundidad(numNacimientos, poblacionFertil));
 }
 
-function calculoPoplacion(n, poblacion, tasa_nat, tasa_mort) {
+function calculoPoplacion(n, p, tasa_nat, tasa_mort) {
     if (n == 0) {
-        return poblacion;
+        return p;
     } else {
-        anhoAnterior = calculoPoplacion(n - 1, poblacion, tasa_nat, tasa_mort);
-        console.log(anhoAnterior);
-        return añoAnterior + anhoAnterior * tasa_nat - anhoAnterior * tasa_mort;
+        anhoAnterior = calculoPoplacion(n - 1, p, tasa_nat, tasa_mort);
+        poblacionActual = Math.round(anhoAnterior + anhoAnterior * tasa_nat - anhoAnterior * tasa_mort);
+        poblacion.push(poblacionActual);
+
+        return poblacionActual;
     }
+}
+
+function calculoFecundidad(numNacimientos, pf) {
+    return numNacimientos / pf * 10;
 }
